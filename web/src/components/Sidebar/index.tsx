62b1a353360bec1fb0ba6a260632d677fbf6e1f5
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2025-01-14 13:32:02 +0100

    extract sidebar

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -21,14 +21,6 @@ import { useNavigate } from 'react-router-dom'
 
 function Sidebar() {
   const [open, setOpen] = useState(false)
-  const navigate = useNavigate()
-  const onArticles = () => {
-    navigate('/articles')
-  }
-  const onLogout = () => {
-    resetAuthJwt()
-    window.location.reload()
-  }
   return (
     <>
       <IconButton variant="text" size="lg" onClick={() => setOpen(!open)}>
@@ -40,43 +32,56 @@ function Sidebar() {
       </IconButton>
 
       <Drawer open={open} onClose={() => setOpen(false)}>
-        <Card className="h-screen ">
-          <div className="mb-2 p-4 flex flex-row items-center justify-center space-x-2 select-none">
-            <img src="/icon.png" alt="logo" className="h-10 w-10" />
-            <Typography variant="h5" color="blue-gray">
-              GoPress
-            </Typography>
-          </div>
-          <List>
-            <ListItem disabled>
-              <ListItemPrefix>
-                <PresentationChartBarIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              Dashboard
-            </ListItem>
-            <ListItem onClick={onArticles}>
-              <ListItemPrefix>
-                <ClipboardDocumentIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              Articles
-            </ListItem>
-            <ListItem disabled>
-              <ListItemPrefix>
-                <Cog6ToothIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              Settings
-            </ListItem>
-            <ListItem onClick={onLogout}>
-              <ListItemPrefix>
-                <PowerIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              Log Out
-            </ListItem>
-          </List>
-        </Card>
+        <SidebarContent />
       </Drawer>
     </>
   )
 }
 
+function SidebarContent() {
+  const navigate = useNavigate()
+  const onArticles = () => {
+    navigate('/articles')
+  }
+  const onLogout = () => {
+    resetAuthJwt()
+    window.location.reload()
+  }
+  return (
+    <Card className="h-screen ">
+      <div className="mb-2 p-4 flex flex-row items-center justify-center space-x-2 select-none">
+        <img src="/icon.png" alt="logo" className="h-10 w-10" />
+        <Typography variant="h5" color="blue-gray">
+          GoPress
+        </Typography>
+      </div>
+      <List>
+        <ListItem disabled>
+          <ListItemPrefix>
+            <PresentationChartBarIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Dashboard
+        </ListItem>
+        <ListItem onClick={onArticles}>
+          <ListItemPrefix>
+            <ClipboardDocumentIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Articles
+        </ListItem>
+        <ListItem disabled>
+          <ListItemPrefix>
+            <Cog6ToothIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Settings
+        </ListItem>
+        <ListItem onClick={onLogout}>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  )
+}
 export default Sidebar
