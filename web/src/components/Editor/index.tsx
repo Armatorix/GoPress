935dea9efdac379a2465c0581b3d70a2aec2927a
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2025-01-14 12:52:33 +0100

    update editor to list only wokring props

--- web/src/components/Editor/index.tsx
+++ web/src/components/Editor/index.tsx
@@ -1,5 +1,6 @@
-import ReactQuill from 'react-quill-new'
+import ReactQuill, { Quill } from 'react-quill-new'
 import 'react-quill-new/dist/quill.snow.css'
+
 type EditorProps = {
   label: string
   value: string
@@ -16,21 +17,29 @@ const Editor: React.FC<EditorProps> = ({ label, value, setValue }) => {
         modules={{
           toolbar: [
             ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-            ['blockquote', 'code-block'],
-            ['link', 'image', 'video', 'formula'],
+            [
+              'blockquote',
+              // 'code-block', NOTE: do not work
+            ],
+            [
+              'link',
+              'image',
+              'video',
+              // 'formula', // NOTE: do not work
+            ],
 
             [{ header: 1 }, { header: 2 }], // custom button values
             [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
-            [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+            // [{ script: 'sub' }, { script: 'super' }], // superscript/subscript: NOTE: do not work
             [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-            [{ direction: 'rtl' }], // text direction
+            // [{ direction: 'rtl' }], // text direction // NOTE: do not work
 
             [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
             [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-            [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+            // [{ color: [] }, { background: [] }], // dropdown with defaults from theme NOTE: do not work
             [{ font: [] }],
-            [{ align: [] }],
+            // [{ align: [] }], NOTE: do not work
 
             ['clean'], // remove formatting button
           ],
