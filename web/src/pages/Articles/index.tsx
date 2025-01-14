53821fc430c89131f628e64a22ab7b17b28287af
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2025-01-14 13:38:41 +0100

    add stylingfor mobile

--- web/src/pages/Articles/index.tsx
+++ web/src/pages/Articles/index.tsx
@@ -38,7 +38,7 @@ type ArticleTileProps = {
 
 function ArticleList({ articles }: { articles: Article[] }) {
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
       {articles.map((article) => (
         <ArticleTile key={article.id} article={article} />
       ))}
